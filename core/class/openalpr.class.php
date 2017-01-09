a56c8d5bf8aaac2047ddff084ec19d23ad850569
Halbout Michael <michaelhalbout@gmail.com>
2017-01-09 13:03:33 +0100

    Ajout d'un regex de validité du format de plaque

--- core/class/openalpr.class.php
+++ core/class/openalpr.class.php
@@ -228,90 +228,100 @@ class openalpr extends eqLogic {
 		$PlateConfigure=false;
 		foreach($Detect["results"] as $Results){
 			foreach($Results["candidates"] as $Plate){
-				$CmdPlates=cmd::byLogicalId(trim($Plate["plate"]));
-				if(is_array($CmdPlates)){
-					foreach($CmdPlates as $CmdPlate){
-						if (is_object($CmdPlate)) {
-							log::add('openalpr','debug','La plaque d\'immatriculation  '.$Plate["plate"].' a ete détécté avec la confidence '.$Plate["confidence"]);
-							$PlateConfigure=true;
-							$CameraAutorise=$CmdPlate->getEqLogic()->getConfiguration('AutoriseCamera');
-							if($CameraAutorise=='all' || $CameraAutorise==$Detect["camera_id"])
-								$CmdPlate->execute($Plate);
-						}
-					} 
-				}
-				if(!$PlateConfigure){
-					$PlateSplite=preg_split('[a-z]',trim($Plate["plate"]));
-					if(count($PlateSplite)== 3){
-						$CmdPlates=cmd::byLogicalId($PlateSplite[0].'****');
-						if(is_array($CmdPlates)){
-							foreach($CmdPlates as $CmdPlate){
-								if (is_object($CmdPlate)) {
-									log::add('openalpr','debug','La plaque d\'immatriculation  '.$Plate["plate"].' a ete détécté avec la confidence '.$Plate["confidence"]);
-									$PlateConfigure=true;
+				if(self::isValideImmat(trim($Plate["plate"]))){
+					$CmdPlates=cmd::byLogicalId(trim($Plate["plate"]));
+					if(is_array($CmdPlates)){
+						foreach($CmdPlates as $CmdPlate){
+							if (is_object($CmdPlate)) {
+								log::add('openalpr','debug','La plaque d\'immatriculation  '.$Plate["plate"].' a ete détécté avec la confidence '.$Plate["confidence"]);
+								$PlateConfigure=true;
+								$CameraAutorise=$CmdPlate->getEqLogic()->getConfiguration('AutoriseCamera');
+								if($CameraAutorise=='all' || $CameraAutorise==$Detect["camera_id"])
 									$CmdPlate->execute($Plate);
-								}
-							} 
-						}
-						$CmdPlates=cmd::byLogicalId($PlateSplite[0].$PlateSplite[1].'**');
-						if(is_array($CmdPlates)){
-							foreach($CmdPlates as $CmdPlate){
-								if (is_object($CmdPlate)) {
-									log::add('openalpr','debug','La plaque d\'immatriculation  '.$Plate["plate"].' a ete détécté avec la confidence '.$Plate["confidence"]);
-									$PlateConfigure=true;
-									$CmdPlate->execute($Plate);
-								}
-							} 
-						}
-						$CmdPlates=cmd::byLogicalId($PlateSplite[0].'**'.$PlateSplite[2]);
-						if(is_array($CmdPlates)){
-							foreach($CmdPlates as $CmdPlate){
-								if (is_object($CmdPlate)) {
-									log::add('openalpr','debug','La plaque d\'immatriculation  '.$Plate["plate"].' a ete détécté avec la confidence '.$Plate["confidence"]);
-									$PlateConfigure=true;
-									$CmdPlate->execute($Plate);
-								}
-							} 
-						}
-						$CmdPlates=cmd::byLogicalId('**'.$PlateSplite[1].'**');
-						if(is_array($CmdPlates)){
-							foreach($CmdPlates as $CmdPlate){
-								if (is_object($CmdPlate)) {
-									log::add('openalpr','debug','La plaque d\'immatriculation  '.$Plate["plate"].' a ete détécté avec la confidence '.$Plate["confidence"]);
-									$PlateConfigure=true;
-									$CmdPlate->execute($Plate);
-								}
-							} 
-						}
-						$CmdPlates=cmd::byLogicalId('**'.$PlateSplite[1].$PlateSplite[2]);
-						if(is_array($CmdPlates)){
-							foreach($CmdPlates as $CmdPlate){
-								if (is_object($CmdPlate)) {
-									log::add('openalpr','debug','La plaque d\'immatriculation  '.$Plate["plate"].' a ete détécté avec la confidence '.$Plate["confidence"]);
-									$PlateConfigure=true;
-									$CmdPlate->execute($Plate);
-								}
-							} 
-						}
-						$CmdPlates=cmd::byLogicalId('****'.$PlateSplite[2]);
-						if(is_array($CmdPlates)){
-							foreach($CmdPlates as $CmdPlate){
-								if (is_object($CmdPlate)) {
-									log::add('openalpr','debug','La plaque d\'immatriculation  '.$Plate["plate"].' a ete détécté avec la confidence '.$Plate["confidence"]);
-									$PlateConfigure=true;
-									$CmdPlate->execute($Plate);
-								}
-							} 
+							}
+						} 
+					}
+					if(!$PlateConfigure){
+						$PlateSplite=preg_split('[a-z]',trim($Plate["plate"]));
+						if(count($PlateSplite)== 3){
+							$CmdPlates=cmd::byLogicalId($PlateSplite[0].'****');
+							if(is_array($CmdPlates)){
+								foreach($CmdPlates as $CmdPlate){
+									if (is_object($CmdPlate)) {
+										log::add('openalpr','debug','La plaque d\'immatriculation  '.$Plate["plate"].' a ete détécté avec la confidence '.$Plate["confidence"]);
+										$PlateConfigure=true;
+										$CmdPlate->execute($Plate);
+									}
+								} 
+							}
+							$CmdPlates=cmd::byLogicalId($PlateSplite[0].$PlateSplite[1].'**');
+							if(is_array($CmdPlates)){
+								foreach($CmdPlates as $CmdPlate){
+									if (is_object($CmdPlate)) {
+										log::add('openalpr','debug','La plaque d\'immatriculation  '.$Plate["plate"].' a ete détécté avec la confidence '.$Plate["confidence"]);
+										$PlateConfigure=true;
+										$CmdPlate->execute($Plate);
+									}
+								} 
+							}
+							$CmdPlates=cmd::byLogicalId($PlateSplite[0].'**'.$PlateSplite[2]);
+							if(is_array($CmdPlates)){
+								foreach($CmdPlates as $CmdPlate){
+									if (is_object($CmdPlate)) {
+										log::add('openalpr','debug','La plaque d\'immatriculation  '.$Plate["plate"].' a ete détécté avec la confidence '.$Plate["confidence"]);
+										$PlateConfigure=true;
+										$CmdPlate->execute($Plate);
+									}
+								} 
+							}
+							$CmdPlates=cmd::byLogicalId('**'.$PlateSplite[1].'**');
+							if(is_array($CmdPlates)){
+								foreach($CmdPlates as $CmdPlate){
+									if (is_object($CmdPlate)) {
+										log::add('openalpr','debug','La plaque d\'immatriculation  '.$Plate["plate"].' a ete détécté avec la confidence '.$Plate["confidence"]);
+										$PlateConfigure=true;
+										$CmdPlate->execute($Plate);
+									}
+								} 
+							}
+							$CmdPlates=cmd::byLogicalId('**'.$PlateSplite[1].$PlateSplite[2]);
+							if(is_array($CmdPlates)){
+								foreach($CmdPlates as $CmdPlate){
+									if (is_object($CmdPlate)) {
+										log::add('openalpr','debug','La plaque d\'immatriculation  '.$Plate["plate"].' a ete détécté avec la confidence '.$Plate["confidence"]);
+										$PlateConfigure=true;
+										$CmdPlate->execute($Plate);
+									}
+								} 
+							}
+							$CmdPlates=cmd::byLogicalId('****'.$PlateSplite[2]);
+							if(is_array($CmdPlates)){
+								foreach($CmdPlates as $CmdPlate){
+									if (is_object($CmdPlate)) {
+										log::add('openalpr','debug','La plaque d\'immatriculation  '.$Plate["plate"].' a ete détécté avec la confidence '.$Plate["confidence"]);
+										$PlateConfigure=true;
+										$CmdPlate->execute($Plate);
+									}
+								} 
+							}
 						}
 					}
 				}
-			}
-			if(!$PlateConfigure && config::byKey('inconnue','openalpr')){
-				$Equipement = openalpr::addEquipement('Plaques détectées inconnu','inconnu');
-				$CmdPlate=openalpr::addCommande($Equipement,$Results["plate"],$Results["plate"]);
-				$CmdPlate->execute($Results);
+				if(!$PlateConfigure && config::byKey('inconnue','openalpr')){
+					$Equipement = openalpr::addEquipement('Plaques détectées inconnu','inconnu');
+					$CmdPlate=openalpr::addCommande($Equipement,$Results["plate"],$Results["plate"]);
+					$CmdPlate->execute($Results);
+				}
 			}
 		}
+	}
+	public static function isValideImmat($plate){
+		if (strlen($plate) <= 9 && preg_match("#^[0-9]{1,4}[A-Z]{1,4}[0-9]{1,2}$#", $plate)) {
+			return true;
+		}elseif  (strlen($var) <= 7 && preg_match("#^[A-Z]{1,2}[0-9]{1,3}[A-Z]{1,2}$#", $plate))  {
+			return true;
+		}
+		return false;
 	}
 	public static function SendLastSnap($file){
 		if (config::byKey('snapshot','openalpr')) {
