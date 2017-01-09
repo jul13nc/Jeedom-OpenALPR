bc1c932e849dc16e1efdb6915c82e88fd1fc1f7d
Halbout Michael <michaelhalbout@gmail.com>
2017-01-09 13:59:17 +0100

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -2,31 +2,13 @@
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
 function openalpr_install() {
-	$Source = dirname(__FILE__) . '/../ressources/Plate';
-    $Destination=dirname(__FILE__) . '/../../../../Video/';
-    if(!file_exists($Destination))
-      mkdir($Destination,0777);
-    $Destination.='openAlpr';
-    if(!file_exists($Destination))
-      mkdir($Destination,0777);
-	if(file_exists($Source)){
-		exec('sudo mv '.$Source.'/* '.$Destination);
-		exec('sudo rm -R '.$Source);
-	}
+	foreach(cmd::byLogicalId('lastdetect','openalpr') as $cmd)
+		$cmd->remove();
 }
 
 function openalpr_update() {
-	$Source = dirname(__FILE__) . '/../ressources/Plate';
-    $Destination=dirname(__FILE__) . '/../../../../Video/';
-    if(!file_exists($Destination))
-      mkdir($Destination,0777);
-    $Destination.='openAlpr';
-    if(!file_exists($Destination))
-      mkdir($Destination,0777);
-	if(file_exists($Source)){
-		exec('sudo mv '.$Source.'/* '.$Destination);
-		exec('sudo rm -R '.$Source);
-	}
+	foreach(cmd::byLogicalId('lastdetect','openalpr') as $cmd)
+		$cmd->remove();
 }
 function openalpr_remove() {
 }
