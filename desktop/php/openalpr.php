d58bdc4aeab36a42733c37adcffad37c0ed414f8
Halbout Michael <michaelhalbout@gmail.com>
2017-09-20 08:39:40 +0200

    Update openalpr.php

--- desktop/php/openalpr.php
+++ desktop/php/openalpr.php
@@ -153,12 +153,15 @@ $eqLogics = eqLogic::byType('openalpr');
 										<option value="all">Toutes</option>
 										<?php
 										$Cameras=config::byKey('configuration','openalpr');
-										if(count($Cameras) && isset($Cameras['id'])){
+										if(is_array($Cameras['id'])){
 											foreach($Cameras['id'] as  $key =>$AlprCamera){
 												if($AlprCamera!=''){
 													echo '<option value="'.$AlprCamera.'">'.$Cameras['name'][$key].'</option>';
 												}
 											}
+										}else{
+											if($Cameras['id']!='')
+												echo '<option value="0">'.$Cameras['name'].'</option>';
 										}
 										?>
 									</select>
