0e61c78b0739821a48ec1bf8a6beaf5904cee929
Halbout Michael <michaelhalbout@gmail.com>
2019-01-12 14:51:01 +0100

    Update openalpr.ajax.php

--- core/ajax/openalpr.ajax.php
+++ core/ajax/openalpr.ajax.php
@@ -5,6 +5,23 @@ try {
 	if (!isConnect('admin')) {
 		throw new Exception(__('401 - Accès non autorisé', __FILE__));
 	}
+	if (init('action') == 'addFileMask') {
+			if (isset($_FILES['FileMask'])){
+				$directory = dirname(__FILE__) . '/../../mask/';
+				if(!file_exists($directory)){
+					exec('sudo mkdir -p '.$directory);
+					exec('sudo chmod 777 -R '.$directory);
+				}
+				$target_file = $directory . basename($_FILES["FileMask"]["name"]);
+				move_uploaded_file($_FILES["FileMask"]["tmp_name"], $target_file);
+				config::save('detection_mask_image', $target_file,'openalpr');
+				ajax::success($target_file);
+
+			}
+
+			ajax::error("Le fichier n'a pas été recu");
+
+		}
 	if (init('action') == 'UpdateStatut') {
 		$Commande=cmd::byId(init('id'));
 		if(is_object($Commande)){
